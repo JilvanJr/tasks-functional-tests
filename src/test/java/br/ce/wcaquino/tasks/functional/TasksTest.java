4e3cfdd8fe7d8472215e50eb38e184a255fe97a8
JilvanJr <jilvanj@hotmail.com>
2021-06-06 20:02:32 -0300

    teste de remover

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -8,15 +8,16 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TasksTest {
 
 	public WebDriver acessarAplicacao() throws MalformedURLException {
-//		WebDriver driver = new ChromeDriver();
-		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.11:4444/wd/hub"), cap);
+		WebDriver driver = new ChromeDriver();
+//		DesiredCapabilities cap = DesiredCapabilities.chrome();
+//		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.11:4444/wd/hub"), cap);
 		driver.navigate().to("http://192.168.0.11:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
@@ -75,6 +76,24 @@ public class TasksTest {
 			driver.findElement(By.id("saveButton")).click();
 			String message = driver.findElement(By.id("message")).getText();
 			Assert.assertEquals("Due date must not be in past", message);
+		} finally {
+			driver.quit();
+		}
+	}
+	
+	@Test
+	public void deveRemoverTarefaComSucesso() throws MalformedURLException {
+		WebDriver driver = acessarAplicacao();
+		try {
+			driver.findElement(By.id("addTodo")).click();
+			driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
+			driver.findElement(By.id("dueDate")).sendKeys("06/12/2021");
+			driver.findElement(By.id("saveButton")).click();
+			String message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Success!", message);
+			driver.findElement(By.linkText("Remove")).click();
+			message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Success!", message);
 		} finally {
 			driver.quit();
 		}
